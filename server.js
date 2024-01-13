785a7895a7c7b1e36cf2cb3e789d099ee62cae97
dev-saiful <saifulcseian@gmail.com>
2024-01-13 19:10:30 +0600

    graphql resolvers added

--- server.js
+++ server.js
@@ -9,7 +9,21 @@ const typesArray = loadFilesSync(path.join(__dirname,"**/*.graphql"));
 
 
 const schema = makeExecutableSchema({
-    typeDefs:typesArray
+    typeDefs:typesArray,
+    resolvers:{
+        Query:{
+            products: async(parent)=>{
+                console.log("Getting Products");
+                const product = await Promise.resolve(parent.products);
+                return product;
+            },
+            orders: async(parent)=>{
+                console.log("Getting Orders");
+                const order = await Promise.resolve(parent.orders);
+                return order;
+            },
+        }
+    }
 });
 
 const root = {
